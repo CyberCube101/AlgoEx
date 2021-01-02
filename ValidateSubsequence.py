6946af83b83d42b242072a1749bc78fe2976d01e
mark banford <markbanford@gmail.com>
2021-01-02 17:28:28 +0000

    SubSequence

---
+++ ValidateSubsequence.py
@@ -0,0 +1,21 @@
+'''Given two non-empty arrays of integers, write a function that determines whether the second array is a
+subsequence of the first
+array=[5,1,22,25,6,-1,8,10]
+sequence=[1,6,-1,10]
+'''
+
+
+def isValidSubsequence(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
+
+
+array = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
+print(isValidSubsequence(array=array, sequence=sequence))

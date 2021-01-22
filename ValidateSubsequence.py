fa3626ec3de60ae437ee9f0fd3199f047d60a29b
markb <markbanford@gmail.com>
2021-01-22 21:20:29 +0000

    23

--- ValidateSubsequence.py
+++ ValidateSubsequence.py
@@ -1,5 +1,5 @@
 '''Given two non-empty arrays of integers, write a function that determines whether the second array is a
-subsequence of the first
+subsequence of the first..
 array=[5,1,22,25,6,-1,8,10]
 sequence=[1,6,-1,10]
 '''
